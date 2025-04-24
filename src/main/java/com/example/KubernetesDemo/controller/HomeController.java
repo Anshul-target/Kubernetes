9040c66468d49dcd3aab513d7a35792c37cf3bca
Anshul <ayushyadav6a@gmail.com>
2025-04-25 01:49:04 +0530

    Initialised the repo

--- src/main/java/com/example/KubernetesDemo/controller/HomeController.java
+++ src/main/java/com/example/KubernetesDemo/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 
     @GetMapping
     public String index(){
-        return "Heello from this side";
+        return "Heello  from this side...";
     }
     @PostMapping("/save")
     public ResponseEntity<Map<String,String>> saveUser(@ModelAttribute @Valid UserDto userDto){
